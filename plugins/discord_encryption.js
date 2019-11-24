e05f29458383c9ffe43488523af8c126fa1d23a5
Vesdii <>
2019-11-24 08:51:06 -0800

    Fixed false decrypt and made major code cleanup
    Fixed accidental decryption of random messages by inserting "uw" in the middle of each cipher. This is resolved after encryption and before decryption.
    Made a major code cleanup so this file doesn't look like a hoarder's house.

--- plugins/discord_encryption.js
+++ plugins/discord_encryption.js
@@ -1,21 +1,17 @@
 // CONFIG: Insert your password here.
 var password = "Khr7pKJNLXd+Zstn3sZ3MIbLm43KeIcc1TeqegCx5WXJ+LY9Ndxp4r+2ic0iV7PmLL1LSifU/Sl5LU8lfnBe6LGyyDV5xk9JdAzmdPrNm05sorwzFXEUjAwkhA+ad9l9mQ2VUeez4hwNkh7SrUnMvQYQwCkBLnpxifRrSFewA3PLMSNQARAQABtBpVbndvdW5kIDxVbndvdW5kQGxtYW8uY29tPokCVAQTAQgAPhYhBFGV9ZeahZfR";
-// var password = "adfaldskfjlkadsf";
 var toggle_on = false;
-var enter_pressed = false;
-var message = '';
 /////////////////////////////////////
 // Replace any messages we manage to decrypt.
 
-const Plugin = require('../plugin');
-var cyph = ""
+const Plugin = require("../plugin");
 
 module.exports = new Plugin({
 
-    name: 'Encryptcord Alpha',
-    author: 'Unwound',
+    name: "Encryptcord Alpha",
+    author: "Unwound",
     description: "Integrated AES encryption - Hit F2 to toggle",
-    color: 'indigo',
+    color: "indigo",
 
     load: function () {
         send_msg();
@@ -23,33 +19,45 @@ module.exports = new Plugin({
     }
 });
 
+function ensure_cypher(cypher, reverse = false) {
+    let mid = cypher.length / 2;
+    if (!reverse) {
+        return cypher.substring(0, mid) + "uw" + cypher.substring(mid);
+    } else {
+        return cypher.substring(0, mid-1) + cypher.substring(mid+1);
+    }
+}
+
 function send_msg() {
-    window.monkeyPatch(findModule('sendMessage'), 'sendMessage', b => {
+    window.monkeyPatch(findModule("sendMessage"), "sendMessage", b => {
         if (toggle_on) {
-            if(getOS() == 'Linux') {
-                var recv = b.methodArguments[1].content.replace("[!]", "")
+            let message = b.methodArguments[1].content;
+            if(getOS() != "Linux") {
+                message = message.replace("🔒", "");
             } else {
-                var recv = b.methodArguments[1].content.replace("🔒", "")
+                message = message.replace("[!]", "");
             }
-            var cypher = CryptoJS.AES.encrypt(recv, password);
-            b.methodArguments[1].content = cypher + " ";
+            let cypher = String(CryptoJS.AES.encrypt(message, password));
+            cypher = ensure_cypher(cypher);
+            b.methodArguments[1].content = cypher;
         }
         return b.callOriginalMethod(b.methodArguments);
     });
 }
 
 function getOS() {
-    var userAgent = window.navigator.userAgent,
+    // userAgent, macosPlatforms, and iosPlatforms are never used
+    let userAgent = window.navigator.userAgent,
         platform = window.navigator.platform,
-        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+        iosPlatforms = ["iPhone", "iPad", "iPod"],
         os = null;
 
     if (windowsPlatforms.indexOf(platform) !== -1) {
-        os = 'Windows';
+        os = "Windows";
     } else if (!os && /Linux/.test(platform)) {
-        os = 'Linux';
+        os = "Linux";
     }
     return os;
 }
@@ -58,42 +66,39 @@ function decryptDiscordMessages(password) {
     setTimeout(function () {
         decryptDiscordMessages(password)
     }, 10);
-    var nodes = document.getElementsByClassName('markup-2BOw-j');
-    for (var i = nodes.length; i--;) {
+    let nodes = document.getElementsByClassName("markup-2BOw-j");
+    for (let i = nodes.length; i--;) {
         if (nodes[i].__decrypted != undefined || nodes[i].__cypher != undefined) continue;
         nodes[i].__decrypted = true;
-        var text = nodes[i].innerText;
-        if (text.endsWith('(edited)')) {
-            text = text.slice(0, -'(edited)'.length);
-        }
-        var decrypted = CryptoJS.AES.decrypt(text, password);
-        if (decrypted == "") continue;
+        let text = nodes[i].innerText;
+        if (text.substring((text.length/2)-1, (text.length/2)+1) != "uw") continue;
+        text = ensure_cypher(text, true)
+        let decrypted = CryptoJS.AES.decrypt(text, password);
         try {
-            var message = decrypted.toString(CryptoJS.enc.Utf8);
+            let message = decrypted.toString(CryptoJS.enc.Utf8);
             if (message == "") continue;
             nodes[i].innerHTML = '<b style="color:PaleTurquoise;font-size:112%"><div style="color:PaleTurquoise;font-size:112%">' + message + '</div></b>';
         } catch (error) {
-            // console.log("Normal Message - Can't Decrypt: " + message); //Try and decrypt
+            console.log("Normal Message - Can't Decrypt: " + message); //Try and decrypt
         }
     }
 }
 decryptDiscordMessages(password);
 
 function registerMessageHook() {
-    if(getOS() == 'Linux') {
-        var lock_ico = "[!]";
-    } else {
-        var lock_ico = "🔒";
+    let lock_ico = "🔒"
+    if(getOS() == "Linux") {
+        let lock_ico = "[!]";
     }
     setTimeout(registerMessageHook, 25);
-    var targetNode = document.getElementsByClassName('textArea-2Spzkt')[0];
+    let targetNode = document.getElementsByClassName("textArea-2Spzkt")[0];
     if (targetNode == undefined) return;
     if (targetNode.value.startsWith(lock_ico) != toggle_on) {
         if (toggle_on) targetNode.value = lock_ico + " " + targetNode.value.trim();
         else targetNode.value = targetNode.value.replace(lock_ico, "");
     }
     targetNode.onkeydown = function(e) {
-        var message = targetNode.value;
+        let message = targetNode.value;
         if (toggle_on) {
             try {
                 targetNode.value = lock_ico + " " + message.replace(lock_ico, "").replace(/^\s+/, "");
