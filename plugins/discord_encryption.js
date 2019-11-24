9d5b8c8caf63af90704b73b6f7f45bb2518bb468
Vesdii <andrew.vesdii@gmail.com>
2019-11-23 19:35:15 -0800

    Whitespace fix and cleanup
    Fixed leading whitespace after toggling off encryption. Also cleaned up a bit of your wretched code.
    
    Changed plugin description and name

--- plugins/discord_encryption.js
+++ plugins/discord_encryption.js
@@ -12,9 +12,9 @@ var cyph = ""
 
 module.exports = new Plugin({
 
-    name: 'PGP Prototype',
+    name: 'Encryptcord Alpha',
     author: 'Unwound',
-    description: "PGP encrytypion - Automated.",
+    description: "Integrated AES encryption - Hit F2 to toggle",
     color: 'indigo',
 
     load: function () {
@@ -26,32 +26,32 @@ module.exports = new Plugin({
 function send_msg() {
     window.monkeyPatch(findModule('sendMessage'), 'sendMessage', b => {
         if (toggle_on) {
-			if(getOS() == 'Linux') {
-				var recv = b.methodArguments[1].content.replace("[!]", "")
-			} else {
-				var recv = b.methodArguments[1].content.replace("🔒", "")
-			}
-			var cypher = CryptoJS.AES.encrypt(recv, password);
-			b.methodArguments[1].content = cypher + " ";
+            if(getOS() == 'Linux') {
+                var recv = b.methodArguments[1].content.replace("[!]", "")
+            } else {
+                var recv = b.methodArguments[1].content.replace("🔒", "")
+            }
+            var cypher = CryptoJS.AES.encrypt(recv, password);
+            b.methodArguments[1].content = cypher + " ";
         }
         return b.callOriginalMethod(b.methodArguments);
     });
 }
 
 function getOS() {
-	var userAgent = window.navigator.userAgent,
-		platform = window.navigator.platform,
-		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-		os = null;
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
 
-	if (windowsPlatforms.indexOf(platform) !== -1) {
-		os = 'Windows';
-	} else if (!os && /Linux/.test(platform)) {
-  		os = 'Linux';
-	}
-	return os;
+    if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
+    return os;
 }
 
 function decryptDiscordMessages(password) {
@@ -80,34 +80,32 @@ function decryptDiscordMessages(password) {
 decryptDiscordMessages(password);
 
 function registerMessageHook() {
-	if(getOS() == 'Linux') {
-    var lock_ico = "[!]";
-	} else {
-    var lock_ico = "🔒";
-	}
+    if(getOS() == 'Linux') {
+        var lock_ico = "[!]";
+    } else {
+        var lock_ico = "🔒";
+    }
     setTimeout(registerMessageHook, 25);
     var targetNode = document.getElementsByClassName('textArea-2Spzkt')[0];
     if (targetNode == undefined) return;
     if (targetNode.value.startsWith(lock_ico) != toggle_on) {
         if (toggle_on) targetNode.value = lock_ico + " " + targetNode.value.trim();
-        else targetNode.value = targetNode.value.replace(lock_ico, "")
+        else targetNode.value = targetNode.value.replace(lock_ico, "");
     }
-    targetNode.onkeydown = function (e) {
+    targetNode.onkeydown = function(e) {
         var message = targetNode.value;
         if (toggle_on) {
             try {
                 targetNode.value = lock_ico + " " + message.replace(lock_ico, "").replace(/^\s+/, "");
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-
-        } else {
-            targetNode.value = message.replace(lock_ico, "")
         }
         if (e.keyCode == 113) {
+            targetNode.value = message.replace(lock_ico, "").replace(/^\s+/, "");
             toggle_on = !toggle_on;
-            console.log("Toggled On:" + toggle_on)
-        };
+            console.log("Toggled:" + toggle_on);
+        }
     }
 }
 registerMessageHook();
